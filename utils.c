d82699a172ac6517e64bfcbffba8c1cb7840215f
CalvinNKM <nkmcalvin@gmail.com>
2025-10-20 15:29:14 +0200

    Print et create cell fonction

--- utils.c
+++ utils.c
@@ -3,22 +3,21 @@
 #include <string.h>
 
 #include "utils.h"
+#include <errno.h>
 
+#include "utils.h"
 static char *getID(int i)
 {
-    // translate from 1,2,3, .. ,500+ to A,B,C,..,Z,AA,AB,...
     static char buffer[10];
     char temp[10];
     int index = 0;
 
-    i--; // Adjust to 0-based index
+    i--;
     while (i >= 0)
     {
         temp[index++] = 'A' + (i % 26);
         i = (i / 26) - 1;
     }
-
-    // Reverse the string to get the correct order
     for (int j = 0; j < index; j++)
     {
         buffer[j] = temp[index - j - 1];
@@ -26,4 +25,28 @@ static char *getID(int i)
     buffer[index] = '\0';
 
     return buffer;
+}
+
+cell *create_cell(int to, float prob)
+{
+    cell *c = (cell*)malloc(sizeof(cell));
+    if (!c) {
+        return NULL;
+    }
+    c->to = to;
+    c->prob = prob;
+    c->next = NULL;
+    return c;
+}
+
+
+void print_list(const list *l)
+{
+    printf("[head @]");
+    const cell *cur = l->head;
+    while (cur) {
+        printf(" -> (%d, %.2f)", cur->to, cur->prob);
+        cur = cur->next;
+    }
+    printf("\n");
 }
--- utils.h
+++ utils.h
@@ -27,7 +27,7 @@ void    free_adj_list(adj_list *g);
 
 void add_edge(adj_list *g, int from, int to, float prob);
 
-void print_list(FILE *out, const list *l);
+void print_list(const list *l);
 void print_adj_list(FILE *out, const adj_list *g);
 
 adj_list readGraph(const char *filename);

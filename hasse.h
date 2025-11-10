af336699e5f97f63203059742f9c85c495fe3bd6
theokng <theo.konig@efrei.net>
2025-11-10 14:39:07 +0100

    data structs part 2

--- hasse.h
+++ hasse.h
@@ -1,6 +1,33 @@
 #ifndef __HASSE_H__
 #define __HASSE_H__
 
+typedef struct {
+    int identifier;
+    int number;
+    int access_number;
+    int processing;
+} t_tarjan_vertex;
+
+typedef struct tarjan_cell {
+    t_tarjan_vertex val;
+    struct tarjan_cell* next;
+}t_tarjan_cell;
+
+typedef struct {
+    int size;
+    t_tarjan_cell* head;
+} t_tarjan_list;
+
+typedef struct {
+    char* name;
+    t_tarjan_cell list;
+} t_class;
+
+typedef struct {
+    int size;
+    t_class *lists;
+}t_partition;
+
 void removeTransitiveLinks(t_link_array *p_link_array);
 
 /**
--- utils.h
+++ utils.h
@@ -3,14 +3,14 @@
 
 #include <stdio.h>
 
-//a vertex
+//an edge
 typedef struct cell {
     int to;
     float prob;
     struct cell *next;
 } cell;
 
-//all outgoing vertices of a node
+//all outgoing edges of a node
 typedef struct {
     cell *head;
 } list;
@@ -21,19 +21,21 @@ typedef struct {
     list *lists;
 } adj_list;
 
-//create a new cell (a vertex)
+
+
+//create a new cell (an edge)
 cell *create_cell(int to, float prob);
 //create the adjacency list
 adj_list create_adj_list(int n);
 
 //add the vertex to the adjacency list
 void addCellToList(list *L, int dest, float prob);
-//create a list containing the outgoing vertices of one node
+//create a list containing the outgoing edges of one node
 list createList(void);
 
-//print the list of all outgoing vertices of one node
+//print the list of all outgoing edges of one node
 void print_list(const list *l);
-//print the list of outgoing vertices for each node
+//print the list of outgoing edges for each node
 void print_adj_list(const adj_list *al);
 
 //read the data of the file into the variables

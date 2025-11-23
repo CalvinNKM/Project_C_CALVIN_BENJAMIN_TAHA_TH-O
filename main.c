de604690011a0e251a48ea6df72e3e8e24b691d8
BenjaminBentze <benjamin.bentze@efrei.net>
2025-11-23 19:16:41 +0100

    Step 1 Part 3 tests

--- main.c
+++ main.c
@@ -43,23 +43,39 @@ int main(void)
     computeClassProperties(p, links);
     free_adj_list(&g);
 
+    printf("\n=== PART 3 ===\n");
     const char *inputFile2 = "../data/exemple_meteo.txt";
     adj_list g2 = readGraph(inputFile2);
-    print_adj_list(&g2);
-    float ** matrix = create_matrix(g2);
-    print_matrix(matrix, g2.size);
 
+    printf("\nHere is the matrix M:\n");
+    float ** matrix = create_matrix(g2);
     float ** matrix2 = copy_matrix(matrix, g2.size);
     print_matrix(matrix2, g2.size);
+
     for (int i=0; i<2; i++) {
         matrix2 = multiply_matrix(matrix2,matrix,g2.size);
     }
+    printf("\nHere is the matrix M^3:\n");
     print_matrix(matrix2, g2.size);
 
     for (int i=0; i<4; i++) {
         matrix2 = multiply_matrix(matrix2,matrix,g2.size);
     }
+    printf("\nHere is the matrix M^7:\n");
     print_matrix(matrix2, g2.size);
+
+    matrix2 = copy_matrix(matrix, g2.size);
+    float ** matrix_temp = initialize_matrix(g2.size);
+    int number = 1;
+    do {
+        matrix_temp = matrix2;
+        matrix2 = multiply_matrix(matrix2,matrix,g2.size);
+        number++;
+    } while (difference_matrix(matrix_temp, matrix2, g2.size) >= 0.01 && number < 100);
+    if (number < 100) {
+        printf("\nThis is the M^n where the difference between the precedent is smaller than 0.01: %d .\n"
+               "The difference is of %f\n",number,difference_matrix(matrix_temp, matrix2, g2.size));
+    }else printf("\nAfter 100 iterations, the difference is still not of 0.01, so it will probably never be.\n");
 
     free_matrix(matrix,g2.size);
 
